9351606a62762fbad8f6cec20c6a5954052530c1
gpnadon <guy-philippe.nadon@calculquebec.ca>
2023-10-11 18:48:45 -0400

    added skeleton for get_final_state function

--- pennylane_snowflurry/snowflurry_converter.py
+++ pennylane_snowflurry/snowflurry_converter.py
@@ -2,7 +2,7 @@ from julia import Snowflurry
 from julia import Main
 import pennylane as qml
 from pennylane.typing import Result, ResultBatch
-
+#https://snowflurrysdk.github.io/Snowflurry.jl/dev/library.html#Snowflurry.sigma_z
 SNOWFLURRY_OPERATION_MAP = {
     # native PennyLane native to snowflurry
     "PauliX": NotImplementedError,
@@ -49,11 +49,11 @@ SNOWFLURRY_OPERATION_MAP = {
 
 
 class PennylaneConverter:
-    def __init__(self, circuit: qml.tape.QuantumScript, rng=None, prng_key=None, debugger=None, interface=None ) -> Result:
+    def __init__(self, circuit: qml.tape.QuantumScript, rng=None, debugger=None, interface=None ) -> Result:
         c = Snowflurry.QuantumCircuit(qubit_count=3)
 
         state, is_state_batched = get_final_state(circuit, debugger=debugger, interface=interface)
-        return self.measure_final_state(circuit, state, is_state_batched, rng=rng, prng_key=prng_key)
+        return self.measure_final_state(circuit, state, is_state_batched, rng=rng)
     def getGateFromCircuit(circuit):
         return list(circuit)
     
@@ -75,3 +75,26 @@ class PennylaneConverter:
 
     def measure_final_state(circuit, state, is_state_batched, rng, prng_key):
         return NotImplementedError
+    
+    def get_final_state(circuit: QuantumTape, debugger=None, interface=None):
+        """
+        Get the final state for the SnowflurryQubitDevice.
+
+        Args:
+            circuit (QuantumTape): The circuit to simulate.
+            debugger (optional): Debugger instance, if debugging is needed.
+            interface (str, optional): The interface to use for any necessary conversions.
+
+        Returns:
+            Tuple[TensorLike, bool]: A tuple containing the final state of the quantum script and
+                a boolean indicating if the state has a batch dimension.
+        """
+        # Process the circuit to extract the final state
+
+        # pseudocode:
+        # state = initialize_state(...)
+        # for op in circuit.operations:
+        #     state = apply_op_to_state(op, state)
+        # is_state_batched = check_if_state_is_batched(state)
+
+        return state, is_state_batched
