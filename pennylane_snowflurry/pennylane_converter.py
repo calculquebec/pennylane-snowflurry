ba11e03a09e2ef27934883a7df0b9c774c2db413
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2024-06-11 13:16:33 -0400

    Refactor pennylane_converter.py: Remove unsupported operations

--- pennylane_snowflurry/pennylane_converter.py
+++ pennylane_snowflurry/pennylane_converter.py
@@ -46,19 +46,10 @@ SNOWFLURRY_OPERATION_MAP = {
     "CRZ": "controlled(rotation_z({2},{0}),[{1}])",
     "PhaseShift": "phase_shift({1},{0})",  # 0 is the angle, 1 is the wire
     "ControlledPhaseShift": "controlled(phase_shift({2},{0}),[{1}])",  # 0 is the angle, 1 is the control qubit, 2 is the target qubit
-    "QubitStateVector": NotImplementedError,
-    "StatePrep": NotImplementedError,
     "Toffoli": "toffoli({0},{1},{2})",
-    "QubitUnitary": NotImplementedError,
-    "U1": NotImplementedError,
-    "U2": NotImplementedError,
     "U3": "universal({3},{0},{1},{2})",
-    "IsingZZ": NotImplementedError,
-    "IsingYY": NotImplementedError,
-    "IsingXX": NotImplementedError,
     "T": "pi_8({0})",
     "Rot": "rotation({3},{0},{1})",  # theta, phi but no omega so we skip {2}, {3} is the wire
-    "QubitUnitary": NotImplementedError,  # might correspond to apply_gate!(state::Ket, gate::Gate) from snowflurry
 }
 
 
