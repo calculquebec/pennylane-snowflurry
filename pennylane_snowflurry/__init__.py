96c1f1214245014e07d06beaa294523c3ce44735
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2024-07-29 22:58:21 -0400

    modified config path contruction for cross-platform compat

--- pennylane_snowflurry/__init__.py
+++ pennylane_snowflurry/__init__.py
@@ -2,12 +2,16 @@ from .julia_setup import JuliaEnv
 from configparser import ConfigParser
 from os import path as os_path
 
-CONFIG_FILE_PATH = os_path.abspath(os_path.dirname(__file__) + "\\..\\env_config.ini")
+# Construct the configuration file path in a platform-independent way
+CONFIG_FILE_PATH = os_path.abspath(
+    os_path.join(os_path.dirname(__file__), "..", "env_config.ini")
+)
 
 # Load the configuration file
 config = ConfigParser()
 config.read(CONFIG_FILE_PATH)
 
+print(config.getboolean("JULIA", "is_user_configured"))
 
 if not config.getboolean("JULIA", "is_user_configured"):
     JuliaEnv().update()
