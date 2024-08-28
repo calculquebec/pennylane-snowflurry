3522fbe0a17bc21efb76252ba06c56d475858d8e
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2024-08-28 16:45:05 -0400

    Created basis for Strategy implementation regarding measurements

---
+++ pennylane_snowflurry/measurements/counts.py
@@ -0,0 +1,6 @@
+from .measurement_process import MeasurementProcess
+
+class Counts(MeasurementProcess):
+
+    def measure(self):
+        pass
---
+++ pennylane_snowflurry/measurements/state.py
@@ -0,0 +1,6 @@
+from .measurement_process import MeasurementProcess
+
+class State(MeasurementProcess):
+
+    def measure(self):
+        pass
