d1757fb35a896c8919ef1950cc30aa702661b23e
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2024-09-09 11:04:27 -0400

    Implemented Strategy design for measurement processes, fixing type conversion for Snowflurry 0.4

--- pennylane_snowflurry/measurements/measurement_process.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class MeasurementProcess(ABC): # TODO find a different name for this class to avoid confusion with Pennylanes's MP
-
-    @abstractmethod
-    def measure(self):
-        pass
