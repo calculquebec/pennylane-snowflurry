14f6204f900521e9c9ca958134a32c1db9573636
Felix Boucher <boucherlfg@gmail.com>
2024-09-18 14:00:05 -0400

    changes to graph util

--- pennylane_snowflurry/graph_util.py
+++ pennylane_snowflurry/graph_util.py
@@ -1,6 +1,19 @@
+from audioop import reverse
+from functools import reduce
+from platform import machine
 import networkx as nx
+from networkx.algorithms import isomorphism
+from networkx.algorithms.isomorphism import vf2userfunc
+from pennylane import qml
 import pennylane.tape as tape
 
+def get_isomorphism(graph : list[list[int]], subgraph : list[list[int]]) -> dict[int, int]:
+    nodes_in_graph = list(set(reduce(lambda a, b: a + b, graph)))
+    nodes_in_subgraph = list(set(reduce(lambda a, b: a + b, subgraph)))
+
+    for node in nodes_in_graph:
+        
+        print(nodes_in_graph)
 
 def circuit_graph(tape : tape.QuantumScript):
     links : list[list[int]] = []
@@ -22,7 +35,7 @@ def circuit_graph(tape : tape.QuantumScript):
 # comment faire un graph qui a plus que 5 liens sur un noeud?
 
 
-def machine_graph(*unavailable : int):
+def machine_graph(broken_nodes : list[int] = [], broken_couplers : list[list[int]] = []):
 #       00
 #       |
 #    08-04-01
@@ -36,7 +49,7 @@ def machine_graph(*unavailable : int):
 #       22-19-15
 #          |
 #          23
-
+    
     links = [[0, 4], [1, 4], [1, 5], [2, 5], [2, 6], 
              [3, 6], [3, 7], [4, 8], [4, 9], 
              [5, 9], [5, 10], [6, 10], [6, 11], 
@@ -46,57 +59,65 @@ def machine_graph(*unavailable : int):
              [14, 18], [14, 19], [15, 19], [16, 20],
              [17, 20], [17, 21], [18, 21], [18, 22],
              [19, 22], [19, 23]]
-    return [i for i in links if i[0] not in unavailable and i[1] not in unavailable] + [list(reversed(i)) for i in links if i[0] not in unavailable and i[1] not in unavailable]
+    return [i for i in links if i[0] not in broken_nodes and i[1] not in broken_nodes \
+            and i not in broken_couplers and list(reversed(i)) not in broken_couplers]
 
 
 class VF2:
-    def __init__(self, pattern : nx.Graph, target : nx.Graph):
-        self.pattern = pattern
-        self.target = target
-        self.pattern_nodes = list(pattern.nodes)
-        self.target_nodes = list(target.nodes)
-        self.matches = {}  # pattern node -> target node mapping
+    _pattern : nx.Graph
+    _target : nx.Graph
+    _pattern_nodes : list[int]
+    _target_nodes : list[int]
+    _matches : dict[int, int]
 
-    def is_valid_mapping(self, pattern_node, target_node):
+    def __init__(self, pattern : nx.Graph, target : nx.Graph):
+        self._pattern = pattern
+        self._target = target
+        self._pattern_nodes = list(pattern.nodes)
+        self._target_nodes = list(target.nodes)
+        self._matches = {}  # pattern node -> target node mapping
+
+    def _is_valid_mapping(self, pattern_node : int, target_node : int) -> bool:
         # Check if the current mapping is valid
-        for p in self.pattern.neighbors(pattern_node):
-            if p in self.matches:
-                t = self.matches[p]
-                if not target_node in self.target.neighbors(t):
+        for p in self._pattern.neighbors(pattern_node):
+            if p in self._matches:
+                t = self._matches[p]
+                if not target_node in self._target.neighbors(t):
                     return False
         return True
 
-    def backtrack(self, pattern_index):
-        if pattern_index == len(self.pattern_nodes):
+    def backtrack(self, pattern_index : int) -> bool:
+        if pattern_index == len(self._pattern_nodes):
             return True
 
-        pattern_node = self.pattern_nodes[pattern_index]
+        pattern_node = self._pattern_nodes[pattern_index]
 
-        for target_node in self.target_nodes:
-            if target_node not in self.matches.values() and self.is_valid_mapping(pattern_node, target_node):
-                self.matches[pattern_node] = target_node
+        for target_node in self._target_nodes:
+            if target_node not in self._matches.values() and self._is_valid_mapping(pattern_node, target_node):
+                self._matches[pattern_node] = target_node
 
                 if self.backtrack(pattern_index + 1):
                     return True
 
-                del self.matches[pattern_node]
+                del self._matches[pattern_node]
 
         return False
 
-    def find_subgraph_isomorphisms(self):
+    def find_subgraph_isomorphisms(self) -> dict[int, int]:
         if self.backtrack(0):
-            return self.matches
+            return self._matches
         else:
             return None
 
-
-def find_isomorphisms(circuit : tape.QuantumScript) -> list[int]:
+def find_isomorphisms(circuit : tape.QuantumScript | list[list[int]], broken_nodes : list[int] = [], broken_couplers : list[list[int]] = []) -> dict[int, int]:
     import networkx as nx
-    circ_graph = nx.Graph(circuit_graph(circuit))
-    machine = nx.Graph(machine_graph())
+
+    circuit = circuit if circuit is list else circuit_graph(circuit)
+    circ_graph = nx.Graph(circuit)
+    machine = nx.Graph(machine_graph(broken_nodes, broken_couplers))
     vf2 = VF2(circ_graph, machine)
     isomorphisms = vf2.find_subgraph_isomorphisms()
-    return [isomorphisms[i] for i in range(len(isomorphisms))]
+    return isomorphisms
     
     
 def subgraph_is_connected(graph : list[list[int]], subgraph_nodes : list[int]):
@@ -114,3 +135,41 @@ def find_path(graph : list[list[int]], start : int, length : int) -> list[list[i
         current = link[1]
         path.append(link)
     return path
+
+def nx_isomoprhism():
+    import networkx as nx
+    import time
+    circuit = [[0, 1], [1, 2], [1, 3]]
+    machine = machine_graph()
+    
+    g_circuit = nx.DiGraph()
+    g_circuit.add_edges_from(circuit)
+    g_circuit = g_circuit.to_undirected()
+    g_machine = nx.DiGraph()
+    g_machine.add_edges_from(machine)
+    g_machine = g_machine.to_undirected()
+
+    print("circuit : " + g_circuit.edges)
+    print("circuit : " + g_machine.edges)
+    iso = nx.isomorphism.GraphMatcher(g_machine, g_circuit)
+    iso.initialize()
+    
+    for match in iso.isomorphisms_iter():
+        print(match)
+
+def find_largest_subgraph_isomorphism(tape, broken_nodes : list[int] = [], broken_couplers : list[list[int]] = []):
+    graph = circuit_graph(tape)
+    from itertools import combinations
+    for i in reversed(range(len(graph) + 1)):
+        for comb in combinations(graph, i):
+            result = find_isomorphisms(comb, broken_nodes, broken_couplers)
+            if result: return result
+
+if __name__ == "__main__":
+    qtape = tape.QuantumTape([qml.CNOT([0, 1]), qml.CNOT([1, 2]), qml.CNOT([2, 0])])
+    result = find_isomorphisms(qtape)
+    if result is None:
+        largest = find_largest_subgraph_isomorphism(qtape)
+        if len(largest) < 3: raise Exception("impossible to simulate connectivity with swaps on a graph that has less than 3 nodes")
+
+    print(result)
